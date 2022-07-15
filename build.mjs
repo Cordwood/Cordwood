a7e254e68cd57ab9312e3467f35a17b38d3bfce4
Beef <beefers@riseup.net>
2022-07-15 18:55:34 +0100

    [Build] Overhaul

--- build.mjs
+++ build.mjs
@@ -1,9 +1,9 @@
 import alias from "esbuild-plugin-alias";
 import { build } from "esbuild";
-import fs from "fs";
+import fs from "fs/promises";
 import path from "path";
 
-const tsconfig = JSON.parse(fs.readFileSync("./tsconfig.json"));
+const tsconfig = JSON.parse(await fs.readFile("./tsconfig.json"));
 const aliases = Object.fromEntries(
     Object.entries(tsconfig.compilerOptions.paths).map(([alias, [target]]) => [
         alias,
@@ -11,15 +11,23 @@ const aliases = Object.fromEntries(
     ])
 );
 
-build({
-    entryPoints: ["./src/index.ts"],
-    outfile: "./dist/index.js",
-    minify: true,
-    bundle: true,
-    format: "iife",
-    external: ["react"],
-    target: "esnext",
-    plugins: [
-        alias(aliases)
-    ]
-})
+try {
+    await build({
+        entryPoints: ["./src/index.ts"],
+        outfile: "./dist/index.js",
+        minify: true,
+        bundle: true,
+        format: "iife",
+        external: ["react"],
+        target: "esnext",
+        plugins: [
+            alias(aliases)
+        ]
+    });
+
+    await fs.appendFile("./dist/index.js", "//# sourceURL=Cordwood");
+    console.log("Build successful!");
+} catch(e) {
+    console.error("Build failed...", e);
+    process.exit(1);
+}
