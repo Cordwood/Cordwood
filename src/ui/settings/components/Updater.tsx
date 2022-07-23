c8f3f40f0934b126bbf9a63770806fec02236a1e
Alyxia Sother <lexisoth2005@gmail.com>
2022-07-23 14:30:51 +0200

    [settings/views/About] Make mockup functional

--- src/ui/settings/components/Updater.tsx
+++ src/ui/settings/components/Updater.tsx
@@ -1,43 +1,67 @@
 import { React } from "@webpack/common";
 import Card from "@uikit/components/Card";
-import classNames from "classnames";
 import connectStores from "@utils/connectStores";
-import ButtonStyles from "@uikit/styles/button.mod.scss";
 import DataStore from "@lib/flux/stores/DataStore";
 import { components } from "@octokit/openapi-types";
+import DataActionCreators from "@lib/flux/actions/DataActionCreators";
 
-class Updater extends React.Component<{ commitData: components["schemas"]["commit"][] }> {
+class Updater extends React.Component<{ commitData: components["schemas"]["commit"][]; latestFetch: number }> {
     static displayName = "Updater";
-    constructor(props = { commitData: [] }) {
+    constructor(props = { commitData: [], latestFetch: 0 }) {
         super(props);
+        if (DataStore.commitData!.length == 0) {
+            DataActionCreators.updateData();
+        }
+    }
+
+    get outdatedData() {
+        const date1 = new Date().getTime();
+        const date2 = new Date(this.props.commitData[0].commit.author!.date!).getTime();
+        const diffTime = Math.abs(date2 - date1);
+        const outdatedDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+        return {
+            outdated: outdatedDays >= 3,
+            outdatedDays,
+        };
     }
 
     render() {
-        return (
-            <Card type={Card.Types.PRIMARY}>
-                <Card type={Card.Types.BRAND}>Last updated 3 days ago</Card>
+        if (this.props.commitData.length > 0)
+            return (
+                <Card type={Card.Types.PRIMARY}>
+                    <Card type={this.outdatedData.outdatedDays > 7 ? Card.Types.DANGER : Card.Types.BRAND}>
+                        {this.outdatedData.outdated
+                            ? `You're ${this.outdatedData.outdatedDays > 7 ? "over a week" : `${Math.round(this.outdatedData.outdatedDays)} days`} out of date! Please update!`
+                            : `Last updated ${Math.round(this.outdatedData.outdatedDays)} day${Math.round(this.outdatedData.outdatedDays) == 1 ? "" : "s"} ago`}
+                    </Card>
 
-                <div
-                    style={{
-                        display: "flex",
-                        flex: "1 1 auto",
-                        alignItems: "stretch",
-                        justifyContent: "flex-start",
-                        flexWrap: "nowrap",
-                        flexDirection: "row",
-                        boxSizing: "border-box",
-                    }}
-                >
-                    <div style={{ marginLeft: 0, flex: "1 1 auto" }}>
-                        <div>Last update:</div>
-                        <div>
-                            <a href={"https://google.com"}>8fba85b</a> by Alyxia Sother: commit message
+                    <div
+                        style={{
+                            display: "flex",
+                            flex: "1 1 auto",
+                            alignItems: "stretch",
+                            justifyContent: "flex-start",
+                            flexWrap: "nowrap",
+                            flexDirection: "row",
+                            boxSizing: "border-box",
+                        }}
+                    >
+                        <div style={{ marginLeft: 0, flex: "1 1 auto" }}>
+                            <div>Last update:</div>
+                            <div>
+                                <a target={"_blank"} href={`https://github.com/Cordwood/Cordwood/commit/${this.props.commitData[0].sha}`}>
+                                    {this.props.commitData[0].sha.substring(0, 7)}
+                                </a>{" "}
+                                by {this.props.commitData[0].commit.author!.name}: {this.props.commitData[0].commit.message}
+                            </div>
                         </div>
+                        {/* FIXME: Why the fuck does this button refresh the page??? */}
+                        {/*<button className={classNames(ButtonStyles.button, ButtonStyles.buttonPrimary)} onClick={() => DataActionCreators.updateData()}>*/}
+                        {/*    Update*/}
+                        {/*</button>*/}
                     </div>
-                    <button className={classNames(ButtonStyles.button, ButtonStyles.buttonPrimary)}>Update</button>
-                </div>
-            </Card>
-        );
+                </Card>
+            );
     }
 }
 
@@ -45,5 +69,6 @@ class Updater extends React.Component<{ commitData: components["schemas"]["commi
 export default connectStores([DataStore], () => {
     return {
         commitData: DataStore.commitData,
+        latestFetch: DataStore.latestFetch,
     };
 })(Updater);
